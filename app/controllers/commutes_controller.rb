a098ba18e4d6b88d7a91945c28c2972a2e6baf88
Kevin Dong <kevindong@users.noreply.github.com>
2016-07-15 19:39:21 -0400

    Improve error checking and views

--- app/controllers/commutes_controller.rb
+++ app/controllers/commutes_controller.rb
@@ -9,7 +9,7 @@ class CommutesController < ApplicationController
       flash[:success] = "Commute started!"
       redirect_to root_url
     else
-      flash[:success] = "Operation failed!"
+      flash[:error] = "Operation failed!"
       redirect_to root_url
     end
   end
@@ -17,10 +17,19 @@ class CommutesController < ApplicationController
   def endcommute
     @commute = Commute.find_by_id(User.find_by_id(params[:id]).currentCommute)
     @commute.endcommute
-    @commute.save
-    current_user.hasActiveCommute = false
-    current_user.currentCommute = nil
-    current_user.save
-    redirect_to root_url
+    if @commute.save
+      current_user.hasActiveCommute = false
+      current_user.currentCommute = nil
+      if current_user.save
+        flash[:success] = "Commute ended!"
+        redirect_to root_url
+      else
+        flash[:error] = "Operation failed!"
+        redirect_to root_url
+      end
+    else
+      flash[:error] = "Operation failed!"
+      redirect_to root_url
+    end
   end
 end
