64e9ba27e7961601d23d8e04613eabf23d5bf18c
Kevin Dong <kevindong@users.noreply.github.com>
2016-07-20 21:48:04 -0400

    Enable high accuracy geolocation

--- app/assets/javascripts/geo.js
+++ app/assets/javascripts/geo.js
@@ -1,6 +1,16 @@
 function getGeo() {
 	// Check to see if the browser supports the GeoLocation API.
 	if (navigator.geolocation) {
+		var options = {
+		  enableHighAccuracy: true,
+		  timeout: 5000,
+		  maximumAge: 0
+		};
+		
+		function error(err) {
+		  console.warn('ERROR(' + err.code + '): ' + err.message);
+		};
+		
 		// Get the location
 		navigator.geolocation.getCurrentPosition(function(position) {
 			var lat = position.coords.latitude;
@@ -8,7 +18,7 @@ function getGeo() {
 
 			document.getElementById("lat").value = lat;
 			document.getElementById("lon").value = lon;
-		});
+		}, error, options);
 	} else {
 		// Print out a message to the user.
 		document.write('Your browser does not support GeoLocation :(');
